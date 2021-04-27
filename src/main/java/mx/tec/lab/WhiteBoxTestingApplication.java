fececb13a5a0759a7303b8bcd1dd2cf63a9b4c2f
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-04-27 16:46:12 -0600

    Initial commit

---
+++ src/main/java/mx/tec/lab/WhiteBoxTestingApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteBoxTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteBoxTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/tec/lab/WhiteBoxTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.tec.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteBoxTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
